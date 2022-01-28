0ed96e33c28284c58ecb120669bd0cb3fa2e88fe
Christopher Sadler <sadlerlogic@gmail.com>
2022-01-27 20:05:07 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+
+    let thankYouCards = []
+    
+    for (let x = 0; x < namesArray.length; x++) {
+    
+    thankYouCards.push(`Thank you, ${namesArray[x]}, for the wonderful ${event} gift!`)
+    
+    }
+    
+    return thankYouCards
+    
+    }
+        function countDown(startingNumber) {
+    
+    while (startingNumber > 0) {
+    
+    console.log(startingNumber);
+    
+    startingNumber -= 1;
+    
+    }
+    
+    console.log(startingNumber);
+    
+    }
+    
